2f427e6a90a6a3675cf3d999ab868dfdc24802c7
Excel <maximefleurant.exe@gmail.com>
2020-08-17 09:50:59 +0200

    qs

--- src/common/components/Article/CodeBrowser/CodeBrowser.tsx
+++ src/common/components/Article/CodeBrowser/CodeBrowser.tsx
@@ -1,5 +1,6 @@
 import SimpleBar from 'simplebar-react';
 import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
 import prismjs from 'prismjs';
 import { useEffect, FunctionComponent } from 'react';
 import { useTheme } from 'emotion-theming';
@@ -54,13 +55,13 @@ const CodeBrowser: TCodeBrowser = ({ language, code, heightAuto }) => {
         </div>
         <div css={barWrap}>
           <SimpleBar css={{ height: '100%' }}>
-            <Fade duration={1500} fraction={0}>
+            <Slide bottom duration={1500} fraction={0.1}>
               <pre css={codeBox}>
                 <code className={language}>
                   <div>{code}</div>
                 </code>
               </pre>
-            </Fade>
+            </Slide>
           </SimpleBar>
         </div>
       </div>
