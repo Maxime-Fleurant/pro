ef693f89cc8b55999d12e96382ca9a28717ff864
Excel <maximefleurant.exe@gmail.com>
2020-08-24 18:50:09 +0200

    fade

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "a6d693a55ba0f939fcd8aac71f93e643"
+    "revision": "76985fbce943f330a44acf5236f0015e"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
